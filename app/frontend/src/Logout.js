e9190beddc6c9a1dea98e37bf0610af379980683
Tito <justin@justintom.com>
2023-10-23 21:22:59 -0700

    Added frontend directory hopefully as not a submodule

---
+++ app/frontend/src/Logout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Logout() {
+  return (
+    <div>
+        <div className="card">
+        <h2>Sign Out</h2>
+        <p>You are signing out of AI Generator</p>
+        <p>Do you want to logout?</p>
+        <button>Log Out</button>
+      </div>
+    </div>
+  )
+}
