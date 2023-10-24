edb643e5bb3bee6aa81be5792edebb794fe00a9c
Justin Tom <41003728+7ito@users.noreply.github.com>
2023-10-24 13:34:51 -0700

    Update Logout.js
    signinging

--- app/frontend/src/Logout.js
+++ app/frontend/src/Logout.js
@@ -6,7 +6,7 @@ export default function Logout() {
     <div>
         <div className="card">
         <h2>Sign Out</h2>
-        <p>You are signinging out of AI Generator</p>
+        <p>You are signing out of AI Generator</p>
         <p>Do you want to logout?</p>
         <button>Log Out</button>
       </div>
