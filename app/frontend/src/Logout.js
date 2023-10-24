1f1a28f33b4ec19284accbdd60ee1cbd46d3f7d6
Tito <justin@justintom.com>
2023-10-24 13:33:17 -0700

    Updated tailwindcss project structure

--- app/frontend/src/Logout.js
+++ app/frontend/src/Logout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+
 export default function Logout() {
   return (
     <div>
         <div className="card">
         <h2>Sign Out</h2>
-        <p>You are signing out of AI Generator</p>
+        <p>You are signinging out of AI Generator</p>
         <p>Do you want to logout?</p>
         <button>Log Out</button>
       </div>
--- app/frontend/tailwind.config.js
+++ app/frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js}',
-    './src/components/**/*.{html,js}'
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
