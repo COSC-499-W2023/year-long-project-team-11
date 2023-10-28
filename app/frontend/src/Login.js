7368e7447018ee6d3128a637dd147b2505b47ad5
lancecrouton <Lance_Sylvurn@yahoo.com>
2023-10-28 12:38:06 -0700

    Update Login.js

--- app/frontend/src/Login.js
+++ app/frontend/src/Login.js
@@ -8,7 +8,7 @@ function Login() {
         <div class="h-screen flex items-center justify-center">
             <div>
             <div className="rounded-lg w-500 h-500" id="main-signin-box">
-                <img className="w-[50%] h-[50%]" src="../img/symbol-user.png"/>
+                <img className="flex items-center justify-center" src={require("./img/symbol-user.png")} height={100} width={70}/>
                 <h2 className="font-bold">Sign In</h2>
 
                 {/* Form (email, password, remember me, and forgot password) */}
@@ -36,7 +36,7 @@ function Login() {
                         />
                     </div>
                     {/* Submission of form */}
-                    <button type="submit">
+                    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" type="submit">
                         Sign In
                     </button>
                     {/* Remember me */}
