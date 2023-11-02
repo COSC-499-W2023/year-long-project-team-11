1fce41e7083573374214e2080012dc21bee33419
Tito <justin@justintom.com>
2023-11-01 23:22:04 -0700

    Changed landing page so that log in and sign up card are links instead of just the text

--- app/frontend/src/Landing.js
+++ app/frontend/src/Landing.js
@@ -7,27 +7,26 @@ export default function Landing() {
     <div className="landing flex justify-center items-center h-screen" >
 
       <div className='flex flex-row gap-9'>
-        <div className=" flex-auto py-3 px-6 border-[3px] border-black rounded-lg bg-[#E2E2E2]">
-          <img className="object-center" src={require("./img/symbol-user.png")} width={260} />
-          <div class="px-6 py-4">
-            <Link to={"/Login"}>
+        <Link to={"/Login"}>
+          <div className=" flex-auto py-3 px-6 border-[3px] border-black rounded-lg bg-[#E2E2E2]">
+            <img className="object-center" src={require("./img/symbol-user.png")} width={260} />
+            <div class="px-6 py-4">
               <div class="font-bold text-xl mb-2 text-center">Log In</div>
-            </Link>
+            </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="flex-auto py-3 px-6 border-[3px] border-black rounded-lg bg-[#E2E2E2]">
-          <img className="object-center" src={require("./img/symbol-user.png")} width={260} />
-          <div class="px-6 py-4">
-            <Link to={"/SignUp"}>
+        <Link to={"/SignUp"}>
+          <div className="flex-auto py-3 px-6 border-[3px] border-black rounded-lg bg-[#E2E2E2]">
+            <img className="object-center" src={require("./img/symbol-user.png")} width={260} />
+            <div class="px-6 py-4">
               <div class="font-bold text-xl mb-2 text-center">Sign Up</div>
-            </Link>
+            </div>
           </div>
-
-        </div>
+        </Link>
       </div>
 
-    </div>
+    </div >
 
   );
 };
