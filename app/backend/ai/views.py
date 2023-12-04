be75440279ea64adfdb14af8f4c47691a0514c98
Tito <justin@justintom.com>
2023-12-03 16:15:42 -0800

    Reverted commits to original state

--- app/backend/ai/views.py
+++ app/backend/ai/views.py
@@ -1,31 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-import json
-import openai
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+# from langchain.embeddings import GPT4AllEmbeddings
 
 # Create your views here.
 
-api_key = ""
-openai.api_key = api_key
-
-def prompt_openai(prompt):
-    response = openai.chat.completions.create(
-        model = "gpt-3.5-turbo",
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens = 50,
-        temperature = 0.7
-    )
-    return response.choices[0].message.content.strip()
+default_template = PromptTemplate.from_template(
+    "This is text taken from university course lecture slides. From this original material, generate text for new slides that can be used to teach a {targetGrade} class. Output in a markdown format, denote a new slide with a tag of your choosing. {prompt}"
+)
 
 @csrf_exempt
 def ai(request):
     if (request.method == 'POST'):
-        data = json.loads(request.body)
-        prompt = data.get('prompt')
-        response = prompt_openai(prompt)
-        return JsonResponse({'prompt': prompt, 'response': response})
+        load_dotenv()
+        uploaded_file = request.FILES.get('file')
+        prompt = request.POST.get('prompt')
+        targetGrade = request.POST.get('targetGrade')
+
+        # extract text
+        pdf_reader = PdfReader(uploaded_file)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+		
+        # Split text into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+		
+        chunks = text_splitter.split_text(text)
+        embeddings = OpenAIEmbeddings()
+        # embeddings = GPT4AllEmbeddings()
+		
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+		
+        if prompt:
+            docs = knowledge_base.similarity_search(default_template.format(targetGrade=targetGrade, prompt=prompt))
+            llm = OpenAI(temperature=0.8)
+            chain = load_qa_chain(llm, chain_type="stuff")
+            response = chain.run(input_documents=docs, question=default_template.format(targetGrade=targetGrade, prompt=prompt))
+            return JsonResponse({'response': response})
     return HttpResponse("Hello World")
--- app/frontend/src/Prompt.js
+++ app/frontend/src/Prompt.js
@@ -3,39 +3,34 @@ import Cookies from "js-cookie";
 
 export default function Prompt() {
   const [prompt, setPrompt] = useState("");
+  const [file, setFile] = useState(null);
+  const [targetGrade, setTargetGrade] = useState("");
   const [output, setOutput] = useState("");
   const csrfToken = Cookies.get("csrftoken");
 
-  const handlePrompt = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(prompt);
-
-    if (prompt.length === 0) {
-      return;
-    }
 
     const formData = new FormData();
     formData.append("file", file);
     formData.append("prompt", prompt);
     formData.append("targetGrade", targetGrade);
 
-    fetch("http://localhost:8000/api", {
+    fetch("http://localhost:8000/api/", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
         "X-CSRFToken": csrfToken,
       },
-      body: JSON.stringify({
-        prompt: prompt,
-      }),
+      body: formData,
     })
       .then((response) => response.json())
-      .then(data => {
+      .then((data) => {
         setOutput(data.response);
       })
-      .catch(error => {
-        console.error('Error: ', error)
-      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
   };
 
   return (
@@ -46,15 +41,46 @@ export default function Prompt() {
             <p>{output}</p>
           </div>
         )}
-        <form onSubmit={handlePrompt}>
+        <form onSubmit={handleSubmit}>
           <div>
-            <input
-              className="border border-black rounded-md min-w-[500px]"
-              type="text"
-              placeholder="Prompt:"
-              value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
-            />
+            <div className="py-2">
+              <label htmlFor="targetGrade" className="px-2">Target Grade Level:</label>
+
+              <select
+                id="targetGrade"
+                value={targetGrade}
+                onChange={(e) => setTargetGrade(e.target.value)}
+                className="bg-white border border-black rounded-sm p-1 "
+              >
+                <option value="" hidden>
+                  Select Grade
+                </option>
+
+                {[...Array(12).keys()].map((grade) => (
+                  <option key={grade + 1} value={grade + 1}>
+                    Grade {grade + 1}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="py-2">
+              <input
+                type="file"
+                onChange={(e) => setFile(e.target.files[0])}
+                accept="application/pdf"
+              />
+            </div>
+
+            <div>
+              <input
+                className="border border-black rounded-md min-w-[500px] px-2"
+                type="text"
+                placeholder="Prompt:"
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+              />
+            </div>
             <div className="mt-[20px]">
               <button
                 className="bg-[#19747E] text-white py-1 rounded hover:bg-[#316268] p-2"
