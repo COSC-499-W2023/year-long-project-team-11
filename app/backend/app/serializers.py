0004563c2ab5decd7a95fbacc3ade29ea6a68f09
lotusbro <90084005+lotusbro@users.noreply.github.com>
2024-01-28 20:38:30 -0800

    Add files via upload

---
+++ app/backend/app/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from app.models import AppUser
+# from django.contrib.auth import get_user_model, authenticate
+from app.models import AppSaveText
+# AppUser = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AppUser
+        fields = ('email', 'username', 'password')
+        # fields = '__all__'
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+# class UserRegisterSerializer(serializers.ModelSerializer):
+#     class Meta: 
+#         model = AppUser
+#         fields = '__all__'
+#     def create(self, clean_data):
+#         user_obj = AppUser.objects.create_user(email = clean_data['email'], password = clean_data['password'])
+#         user_obj.username = clean_data['username']
+#         user_obj.save()
+#         return user_obj
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def check_user(self, clean_data):
+        user = authenticate(username = clean_data['email'], password = clean_data['password'])
+        if not user:
+            raise ValidaitonError('User not found')
+        return user
+
+class AppSaveTextSerizalizer(serializers.ModelSerializer):
+   class Meta:
+       model= AppSaveText
+       fields=('savecontent',)
+
---
+++ app/backend/app/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from app.models import AppUser
+from app.models import AppSaveText
+from .serializers import UserSerializer
+from .serializers import AppSaveTextSerizalizer
+import os
+import sys
+
+# Create your views here.
+@api_view(['GET'])
+def getData(request):
+    print("In the get(GET) method\n", file=sys.stderr)
+    users = AppUser.objects.all()
+    serializer = UserSerializer(users, many = True)
+    # person = {'username': 'John', 'email': 'johnny@gmail.com'}
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addUser(request):
+    print("In the add(POST) method\n", file=sys.stderr)
+    serializer = UserSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+    else:
+         print(serializer.errors, file=sys.stderr)
+    return Response(serializer.data)
+
+# permission_classes = (IsAuthenticated)
+# @api_view(['GET'])
+# def HomeView(request):
+#     content = {'message': 'Welcome to the JWT Authentication page using React Js and Django!'}
+#     return Response(content)
+class HomeView(APIView):
+    permission_classes = (IsAuthenticated, )
+    def get(self, request):
+        content = {'message': 'Welcome to the JWT Authentication page using React Js and Django!'}
+        return Response(content)
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+    def post(self, request):     
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+def saveOutput(request):
+    try:
+        serializer = AppSaveTextSerizalizer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({"message": "Output saved successfully"}, status=200)
+        else:
+            return JsonResponse(serializer.errors, status=400)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
