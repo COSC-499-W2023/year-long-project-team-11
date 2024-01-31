ae677535ba80a40e2036e392d386f1d3d59f91a0
RyBowler <ry.bowler@gmail.com>
2024-01-31 02:05:42 -0800

    Nav bars now added to pages that had them deleted. Profile page lacking a navbar as the link seems to have been deleted on accident

--- app/frontend/src/SavedContent.js
+++ app/frontend/src/SavedContent.js
@@ -30,23 +30,46 @@ const SavedContent = () => {
   }, [data]);
 
   return (
-    <div className="savedcontent flex flex-col items-center justify-center min-h-screen">
-      <div className="max-w-3xl w-full p-8 bg-white rounded-lg shadow-lg">
-        <h1 className="text-4xl font-bold mb-4">Title of Content</h1>
-        <p className="text-lg mb-4">Content Type</p>  
-        <div className="my-8">
-          <p>{paragraph}</p>
-        </div>
-        <div className="flex justify-between items-center">
-        <div className='buttons flex flex-row'>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-md mr-4" onClick={() => window.location.href = downloadLink}>Download</button>
-        <button className="px-4 py-2 bg-gray-500 text-white rounded-md">Share</button>
-        </div>
-          
+    <div>
+      {/* Nav Bar */}
+      <nav class="bg-[#E2E2E2]">
+          <div class="flex justify-between mr-5 ml-5 py-2">
+              {/* General Area (Left side) */}
+              <div class="flex items-center space-x-1">
+                  <div class="font-bold">(Logo) EduSynth</div>
+                  <a className="text-[#44566B] py-3 px-3 hover:text-black" href="/Prompt">A.I. Page</a>
+                  <a className="bg-[#316268] text-white py-3 px-3 rounded hover:bg-[#3e7a82]" href="/SavedContent">Saved Content</a>
+                  <a className="text-[#44566B] py-3 px-3 hover:text-black" href="/Tutorial">Tutorial</a>
+              </div>
+
+              {/* User Area (Right side) */}
+              <div class="flex items-center space-x-1">
+                  <a className="text-[#44566B] py-3 px-3 hover:text-black" href="/Profile">Profile</a>
+                  <a className="text-[#44566B] py-3 px-3 hover:text-black" href="/Login">Log In</a>
+                  <a className="text-[#44566B] py-3 px-3 hover:text-black" href="/Logout">Log Out</a>
+                  <a className="text-[#44566B] py-3 px-3 hover:text-black" href="/SignUp">Sign Up</a>
+              </div>
+          </div>
+      </nav>
+
+      {/* Content */}
+      <div className="savedcontent flex flex-col items-center justify-center min-h-screen">
+        <div className="max-w-3xl w-full p-8 bg-white rounded-lg shadow-lg">
+          <h1 className="text-4xl font-bold mb-4">Title of Content</h1>
+          <p className="text-lg mb-4">Content Type</p>  
+          <div className="my-8">
+            <p>{paragraph}</p>
+          </div>
+          <div className="flex justify-between items-center">
+          <div className='buttons flex flex-row'>
+          <button className="px-4 py-2 bg-blue-500 text-white rounded-md mr-4" onClick={() => window.location.href = downloadLink}>Download</button>
+          <button className="px-4 py-2 bg-gray-500 text-white rounded-md">Share</button>
+          </div>
+            
+          </div>
         </div>
       </div>
     </div>
-    
   );
 };
 
