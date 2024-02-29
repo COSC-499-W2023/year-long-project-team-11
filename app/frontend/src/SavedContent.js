d599ef3ac5483ce01e96af3a91edbaa63cb95ed8
7ito <justin@justintom.com>
2024-02-28 18:20:40 -0800

    Include MoonLoader for loading visual for Regenerate

--- app/frontend/src/SavedContent.js
+++ app/frontend/src/SavedContent.js
@@ -81,7 +81,6 @@ const SavedContent = () => {
               <button className="px-4 py-2 bg-blue-500 text-white rounded-md mr-4" onClick={() => window.location.href = `http://localhost:8000/api/presentations/${filename}?download=true`}>Download</button>
               <button className="px-4 py-2 bg-gray-500 text-white rounded-md">Share</button>
             </div>
-
           </div>
         </div>
       </div>
